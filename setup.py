1f5cea2c272c73fedc524026a5cb7334193bc425
Régis Behmo <regis@behmo.com>
2022-10-24 15:14:17 +0200

    feat: upgrade to olive

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     python_requires=">=3.7",
-    install_requires=["tutor>=14.0.0,<15.0.0"],
+    install_requires=["tutor>=15.0.0,<16.0.0"],
     entry_points={
         "tutor.plugin.v1": [
             "android = tutorandroid.plugin"
